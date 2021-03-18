4bfbe9afed1f29b9435b8222972c923ab3d2f850
Laura Serrano _ Keajse <lajusega@gmail.com>
2021-03-17 20:04:55 -0500

    Algoritmo con arreglos

--- Algoritmo3.py
+++ Algoritmo3.py
@@ -1,1 +1,21 @@
 #Un profesor necesita obtener el nombre de cada estudiante y sus 5 respectivas notas y debe mostrar la lsta de los estudiantes con su respectiva nota final.
+
+names = []
+grades = []
+
+while True:
+    names.append(input("ingrese el nombre del estudiante: "))
+    average= 0
+    for i in range(1,6):
+        average += float(input(f"Ingrese la nota {i}: "))
+    average = average/5
+    grades.append(average)
+    stopProgram = input("Desear para el programa (s- Si, otra tecla no)")
+    if stopProgram == "s" or stopProgram == "S":
+        break
+for j in range(len(names)):
+   lista = {
+       "Nombre: ": names[j],
+       "Promedio: ": grades[j]
+   }
+   print(lista)
